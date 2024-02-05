1bf46122b127e83962a22cc0857f68c914e2424f
filipcro1 <filip.kolak@gmail.com>
2024-02-05 12:37:41 +0100

    Upload

--- data.js
+++ data.js
@@ -3,9 +3,6 @@ var APP_DATA = {
     {
       "id": "0-blagovaonica",
       "name": "Blagovaonica",
-      "mapcontainer": "1",
-      "x": "11%",
-      "y": "11%",
       "levels": [
         {
           "tileSize": 256,
@@ -50,9 +47,6 @@ var APP_DATA = {
     {
       "id": "1-dnevni-boravak",
       "name": "Dnevni boravak",
-      "mapcontainer": "1",
-      "x": "22%",
-      "y": "22%",
       "levels": [
         {
           "tileSize": 256,
@@ -91,9 +85,6 @@ var APP_DATA = {
     {
       "id": "2-kuhinja",
       "name": "Kuhinja",
-      "mapcontainer": "1",
-      "x": "55%",
-      "y": "55%",
       "levels": [
         {
           "tileSize": 256,
@@ -128,141 +119,11 @@ var APP_DATA = {
         }
       ],
       "infoHotspots": []
-    },
-    {
-      "id": "3-blagovaonica2",
-      "name": "Blagovaonica222-data.js file",
-      "mapcontainer": "2",
-      "x": "45%",
-      "y": "20%",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        },
-        {
-          "tileSize": 512,
-          "size": 2048
-        }
-      ],
-      "faceSize": 2048,
-      "initialViewParameters": {
-        "yaw": -0.2427391700754704,
-        "pitch": 0.21375828738680092,
-        "fov": 1.3545525999327719
-      },
-      "linkHotspots": [
-        {
-          "yaw": 1.1026756493697825,
-          "pitch": 0.11446346998144108,
-          "rotation": 0,
-          "target": "4-dnevni-boravak2"
-        },
-        {
-          "yaw": 2.9534514454386525,
-          "pitch": 0.15235999911186582,
-          "rotation": 0,
-          "target": "5-kuhinja2"
-        }
-      ],
-      "infoHotspots": []
-    },
-    {
-      "id": "4-dnevni-boravak2",
-      "name": "Dnevni boravak222-data.js file",
-      "mapcontainer": "2",
-      "x": "53%",
-      "y": "20%",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        },
-        {
-          "tileSize": 512,
-          "size": 2048
-        }
-      ],
-      "faceSize": 2048,
-      "initialViewParameters": {
-        "yaw": -1.3786462123409962,
-        "pitch": 0.16401648586669282,
-        "fov": 1.3545525999327719
-      },
-      "linkHotspots": [
-        {
-          "yaw": 0.03533328323180385,
-          "pitch": 0.07639054678202406,
-          "rotation": 0,
-          "target": "3-blagovaonica2"
-        }
-      ],
-      "infoHotspots": []
-    },
-    {
-      "id": "5-kuhinja2",
-      "name": "Kuhinja222-data.js file",
-      "mapcontainer": "2",
-      "x": "49%",
-      "y": "29%",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        },
-        {
-          "tileSize": 512,
-          "size": 2048
-        }
-      ],
-      "faceSize": 2048,
-      "initialViewParameters": {
-        "yaw": 0.004115623355176012,
-        "pitch": 0.027973504352811318,
-        "fov": 1.3545525999327719
-      },
-      "linkHotspots": [
-        {
-          "yaw": 0.07195616431882534,
-          "pitch": 0.11584978257795697,
-          "rotation": 0,
-          "target": "3-blagovaonica2"
-        }
-      ],
-      "infoHotspots": []
     }
   ],
   "name": "Virtualna šetnja",
   "settings": {
-    "mouseViewMode": "drag",
-    "deviceOrientationEnabled": true,
+    "mouseViewMode": "qtvr",
     "autorotateEnabled": true,
     "fullscreenButton": true,
     "viewControlButtons": false
